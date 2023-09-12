b4edf651b7a98ac41d78c497a8a2d20b0c668c0a
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-09-12 15:46:32 -0300

    Update final

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -55,7 +55,9 @@ function App() {
     setTodos(newTodos)
   }
 
-  const [search, setSearch] = useState ('')
+  const [search, setSearch] = useState ('');
+  const [filter, setFilter] = useState ('All');
+  const [sort, setSort] = useState ('Asc');
 
 
 
@@ -68,9 +70,16 @@ function App() {
     <div className='app'>
       <h1>Lista de Tarefas</h1>
       <Search search = {search} setSearch = {setSearch} />
-      <Filter/>
+      <Filter filter = {filter} setFilter = {setFilter} setSort = {setSort} />
       <div className='todo-list'>
-        {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (  
+        {todos
+        .filter((todo) => filter ==='All' ? true : filter === 'Completed' ? todo.isCompleted : !todo.isCompleted)
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        .sort((a,b)=> sort === 'Asc' 
+        ? a.text.localeCompare(b.text) 
+        : b.text.localeCompare(a.text)
+        )
+        .map((todo) => (  
          <Todo key={todo.id} todo = {todo} removeTodo = {removeTodo} completeTodo = {completeTodo} />
        ))}
 
--- todo/src/components/Filter.jsx
+++ todo/src/components/Filter.jsx
@@ -1,13 +1,13 @@
 
 
-const Filter = () => {
+const Filter = ({filter, setFilter, setSort}) => {
   return (
     <div className="filter">
         <h2>Filtrar:</h2>
         <div className="filter-options">
             <div>
             <p>Status:</p>
-            <select>
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
                 <option value="All">Todas</option>
                 <option value="Completed">Completas</option>
                 <option value="Incomplete">Incompletas</option>
@@ -15,8 +15,8 @@ const Filter = () => {
         </div>
     <div>
         <p>Ordem alfabética:</p>
-        <button>Asc</button>
-        <button>Desc</button>
+        <button onClick={() => setSort ('Asc')}>+</button>
+        <button onClick={() => setSort ('Desc')}>-</button>
     </div>
     </div>
     </div>
